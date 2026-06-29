8adfa73227ca6207c5a5106324de3e9cffb97bd9
anelisgh <anelis.gheorghias94@gmail.com>
2026-06-29 16:29:32 +0300

    db

--- db-init/chat_service.sql
+++ db-init/chat_service.sql
@@ -43,7 +43,7 @@ CREATE TABLE `conversatii` (
   KEY `idx_conversatii_ultimul_mesaj_la` (`ultimul_mesaj_la`),
   KEY `idx_conv_participants` (`pacient_keycloak_id`,`terapeut_keycloak_id`),
   KEY `idx_conv_ultim_mesaj` (`ultimul_mesaj_la`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -52,10 +52,9 @@ CREATE TABLE `conversatii` (
 
 LOCK TABLES `conversatii` WRITE;
 /*!40000 ALTER TABLE `conversatii` DISABLE KEYS */;
-INSERT INTO `conversatii` (`id`, `created_at`, `pacient_keycloak_id`, `terapeut_keycloak_id`, `ultimul_mesaj_la`, `updated_at`) VALUES (5,'2026-06-07 18:00:00.000000','a2351fcd-9e0c-4ebc-b6fb-2530c9892556','51abc54c-0c2f-4f0a-aead-bdaa9627661e','2026-06-07 20:35:00.000000','2026-06-07 20:35:00.000000');
+INSERT INTO `conversatii` VALUES (5,'2026-06-07 18:00:00.000000','a2351fcd-9e0c-4ebc-b6fb-2530c9892556','51abc54c-0c2f-4f0a-aead-bdaa9627661e','2026-06-07 20:35:00.000000','2026-06-07 20:35:00.000000'),(6,'2026-06-28 15:00:16.727138','00179244-a86e-4ade-8e22-3d70af6de57d','05e0f10e-2c4d-405e-abac-5ccedb83d2af','2026-06-29 11:58:21.901893','2026-06-29 11:58:21.925200');
 /*!40000 ALTER TABLE `conversatii` ENABLE KEYS */;
 UNLOCK TABLES;
-
 
 --
 -- Table structure for table `mesaje`
@@ -80,7 +79,7 @@ CREATE TABLE `mesaje` (
   KEY `idx_mesaj_conv_list` (`conversatie_id`,`trimis_la`),
   KEY `idx_mesaje_conversatie_id_trimis_la` (`conversatie_id`,`trimis_la`),
   KEY `idx_mesaje_expeditor_keycloak_id` (`expeditor_keycloak_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -89,14 +88,9 @@ CREATE TABLE `mesaje` (
 
 LOCK TABLES `mesaje` WRITE;
 /*!40000 ALTER TABLE `mesaje` DISABLE KEYS */;
-INSERT INTO `mesaje` (`id`, `citit_la`, `continut`, `conversatie_id`, `este_citit`, `expeditor_keycloak_id`, `tip_expeditor`, `trimis_la`) VALUES (15,'2026-06-07 20:10:00.000000','Bună seara, domnule terapeut! Aș dori să reprogramez ședința de mâine (luni, 8 iunie) pentru marți, 9 iunie, dacă este posibil.',5,b'1','a2351fcd-9e0c-4ebc-b6fb-2530c9892556','PACIENT','2026-06-07 18:00:00.000000'),(16,'2026-06-07 20:25:00.000000','Bună seara, Sabrina! Sigur că da. Te pot programa marți la ora 10:00. Este în regulă?',5,b'1','51abc54c-0c2f-4f0a-aead-bdaa9627661e','TERAPEUT','2026-06-07 20:15:00.000000'),(17,'2026-06-07 20:30:00.000000','Da, este perfect! Vă mulțumesc frumos.',5,b'1','a2351fcd-9e0c-4ebc-b6fb-2530c9892556','PACIENT','2026-06-07 20:30:00.000000'),(18,'2026-06-07 20:36:00.000000','Cu drag! Am operat modificarea în sistem. Ne vedem marți.',5,b'1','51abc54c-0c2f-4f0a-aead-bdaa9627661e','TERAPEUT','2026-06-07 20:35:00.000000');
+INSERT INTO `mesaje` VALUES (15,'2026-06-07 20:10:00.000000','Bună seara, domnule terapeut! Aș dori să reprogramez ședința de mâine (luni, 8 iunie) pentru marți, 9 iunie, dacă este posibil.',5,_binary '','a2351fcd-9e0c-4ebc-b6fb-2530c9892556','PACIENT','2026-06-07 18:00:00.000000'),(16,'2026-06-07 20:25:00.000000','Bună seara, Sabrina! Sigur că da. Te pot programa marți la ora 10:00. Este în regulă?',5,_binary '','51abc54c-0c2f-4f0a-aead-bdaa9627661e','TERAPEUT','2026-06-07 20:15:00.000000'),(17,'2026-06-07 20:30:00.000000','Da, este perfect! Vă mulțumesc frumos.',5,_binary '','a2351fcd-9e0c-4ebc-b6fb-2530c9892556','PACIENT','2026-06-07 20:30:00.000000'),(18,'2026-06-07 20:36:00.000000','Cu drag! Am operat modificarea în sistem. Ne vedem marți.',5,_binary '','51abc54c-0c2f-4f0a-aead-bdaa9627661e','TERAPEUT','2026-06-07 20:35:00.000000'),(19,'2026-06-28 15:00:19.427591','test',6,_binary '','05e0f10e-2c4d-405e-abac-5ccedb83d2af','TERAPEUT','2026-06-28 15:00:16.948520'),(20,'2026-06-28 15:00:39.084645','buna ziua!',6,_binary '','00179244-a86e-4ade-8e22-3d70af6de57d','PACIENT','2026-06-28 15:00:38.956711'),(21,'2026-06-28 15:02:09.759644','test2',6,_binary '','05e0f10e-2c4d-405e-abac-5ccedb83d2af','TERAPEUT','2026-06-28 15:02:09.402395'),(22,'2026-06-28 15:02:21.213284','test3',6,_binary '','00179244-a86e-4ade-8e22-3d70af6de57d','PACIENT','2026-06-28 15:02:20.995292'),(23,'2026-06-28 15:08:44.105595','test4',6,_binary '','05e0f10e-2c4d-405e-abac-5ccedb83d2af','TERAPEUT','2026-06-28 15:08:19.834378'),(24,'2026-06-28 15:08:55.287355','test5',6,_binary '','05e0f10e-2c4d-405e-abac-5ccedb83d2af','TERAPEUT','2026-06-28 15:08:55.136261'),(25,'2026-06-28 15:09:58.698763','test6',6,_binary '','05e0f10e-2c4d-405e-abac-5ccedb83d2af','TERAPEUT','2026-06-28 15:09:58.521953'),(26,'2026-06-29 11:58:18.220621','test7',6,_binary '','05e0f10e-2c4d-405e-abac-5ccedb83d2af','TERAPEUT','2026-06-29 11:58:13.873521'),(27,'2026-06-29 11:58:22.264134','test8',6,_binary '','00179244-a86e-4ade-8e22-3d70af6de57d','PACIENT','2026-06-29 11:58:21.901892');
 /*!40000 ALTER TABLE `mesaje` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping data for table `mesaje`
---
-
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -107,4 +101,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-05-13 13:39:54
+-- Dump completed on 2026-06-29 16:28:21
--- db-init/servicii_service.sql
+++ db-init/servicii_service.sql
@@ -35,12 +35,12 @@ CREATE TABLE `servicii` (
   `active` bit(1) NOT NULL,
   `created_at` datetime(6) NOT NULL,
   `durata_minute` int NOT NULL,
-  `nume` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `nume` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `pret` decimal(10,2) NOT NULL,
   `updated_at` datetime(6) NOT NULL,
   `tip_serviciu_id` bigint NOT NULL,
-  `created_by` varchar(36) DEFAULT 'SYSTEM',
-  `last_modified_by` varchar(36) DEFAULT 'SYSTEM',
+  `created_by` varchar(36) COLLATE utf8mb4_unicode_ci DEFAULT 'SYSTEM',
+  `last_modified_by` varchar(36) COLLATE utf8mb4_unicode_ci DEFAULT 'SYSTEM',
   PRIMARY KEY (`id`),
   KEY `idx_servicii_tip` (`tip_serviciu_id`),
   KEY `idx_serviciu_nume` (`nume`),
@@ -54,7 +54,7 @@ CREATE TABLE `servicii` (
 
 LOCK TABLES `servicii` WRITE;
 /*!40000 ALTER TABLE `servicii` DISABLE KEYS */;
-INSERT INTO `servicii` (`id`, `active`, `created_at`, `durata_minute`, `nume`, `pret`, `updated_at`, `tip_serviciu_id`) VALUES (1,_binary '','2026-02-28 11:49:25.270994',30,'Evaluare Initiala',150.00,'2026-03-23 18:49:05.000000',1),(2,_binary '','2026-02-28 11:49:37.315817',30,'Reevaluare',100.00,'2026-03-23 18:47:08.000000',1),(6,_binary '','2026-03-23 18:51:51.000000',50,'Ședință individuală',100.00,'2026-03-23 18:51:51.000000',4),(7,_binary '','2026-03-23 18:51:51.000000',60,'Recuperare post-operatorie',150.00,'2026-03-23 18:51:51.000000',4),(8,_binary '','2026-03-23 18:51:51.000000',50,'Gimnastică profilactică',110.00,'2026-03-23 18:51:51.000000',4),(9,_binary '','2026-03-23 18:52:35.000000',20,'Electroterapie (TENS)',50.00,'2026-03-23 18:52:35.000000',5),(10,_binary '','2026-03-23 18:52:35.000000',15,'Terapie cu Ultrasunete',60.00,'2026-03-23 18:52:35.000000',5),(11,_binary '','2026-03-23 18:52:35.000000',10,'Laserterapie',50.00,'2026-03-23 18:52:35.000000',5),(12,_binary '','2026-03-23 18:54:12.000000',15,'Aplicare Kinesio Taping',40.00,'2026-03-23 18:54:12.000000',6),(13,_binary '','2026-03-23 18:54:12.000000',30,'Terapie TECAR',150.00,'2026-03-23 18:54:12.000000',6),(14,_binary '','2026-03-23 18:54:16.000000',30,'Masaj terapeutic regional',90.00,'2026-03-23 18:54:16.000000',7),(15,_binary '','2026-03-23 18:54:16.000000',50,'Drenaj limfatic manual',130.00,'2026-03-23 18:54:16.000000',7);
+INSERT INTO `servicii` VALUES (1,_binary '','2026-02-28 11:49:25.270994',30,'Evaluare Initiala',150.00,'2026-03-23 18:49:05.000000',1,'SYSTEM','SYSTEM'),(2,_binary '','2026-02-28 11:49:37.315817',30,'Reevaluare',100.00,'2026-03-23 18:47:08.000000',1,'SYSTEM','SYSTEM'),(6,_binary '','2026-03-23 18:51:51.000000',50,'Ședință individuală',100.00,'2026-03-23 18:51:51.000000',4,'SYSTEM','SYSTEM'),(7,_binary '','2026-03-23 18:51:51.000000',60,'Recuperare post-operatorie',150.00,'2026-03-23 18:51:51.000000',4,'SYSTEM','SYSTEM'),(8,_binary '','2026-03-23 18:51:51.000000',50,'Gimnastică profilactică',110.00,'2026-03-23 18:51:51.000000',4,'SYSTEM','SYSTEM'),(9,_binary '','2026-03-23 18:52:35.000000',20,'Electroterapie (TENS)',50.00,'2026-03-23 18:52:35.000000',5,'SYSTEM','SYSTEM'),(10,_binary '','2026-03-23 18:52:35.000000',15,'Terapie cu Ultrasunete',60.00,'2026-03-23 18:52:35.000000',5,'SYSTEM','SYSTEM'),(11,_binary '','2026-03-23 18:52:35.000000',10,'Laserterapie',50.00,'2026-03-23 18:52:35.000000',5,'SYSTEM','SYSTEM'),(12,_binary '','2026-03-23 18:54:12.000000',15,'Aplicare Kinesio Taping',40.00,'2026-03-23 18:54:12.000000',6,'SYSTEM','SYSTEM'),(13,_binary '','2026-03-23 18:54:12.000000',30,'Terapie TECAR',150.00,'2026-03-23 18:54:12.000000',6,'SYSTEM','SYSTEM'),(14,_binary '','2026-03-23 18:54:16.000000',30,'Masaj terapeutic regional',90.00,'2026-03-23 18:54:16.000000',7,'SYSTEM','SYSTEM'),(15,_binary '','2026-03-23 18:54:16.000000',50,'Drenaj limfatic manual',130.00,'2026-03-23 18:54:16.000000',7,'SYSTEM','SYSTEM');
 /*!40000 ALTER TABLE `servicii` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -69,11 +69,11 @@ CREATE TABLE `tip_serviciu` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `active` bit(1) NOT NULL,
   `created_at` datetime(6) NOT NULL,
-  `descriere` text COLLATE utf8mb4_unicode_ci,
-  `nume` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descriere` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `nume` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `updated_at` datetime(6) NOT NULL,
-  `created_by` varchar(36) DEFAULT 'SYSTEM',
-  `last_modified_by` varchar(36) DEFAULT 'SYSTEM',
+  `created_by` varchar(36) COLLATE utf8mb4_unicode_ci DEFAULT 'SYSTEM',
+  `last_modified_by` varchar(36) COLLATE utf8mb4_unicode_ci DEFAULT 'SYSTEM',
   PRIMARY KEY (`id`),
   UNIQUE KEY `UKb3q5assjdgtplxgwp1pfr5qqx` (`nume`),
   KEY `idx_tip_serviciu_nume` (`nume`)
@@ -86,7 +86,7 @@ CREATE TABLE `tip_serviciu` (
 
 LOCK TABLES `tip_serviciu` WRITE;
 /*!40000 ALTER TABLE `tip_serviciu` DISABLE KEYS */;
-INSERT INTO `tip_serviciu` (`id`, `active`, `created_at`, `descriere`, `nume`, `updated_at`) VALUES (1,_binary '','2026-02-28 11:47:32.489525','Punctul de plecare pentru orice program de recuperare.','Evaluare','2026-03-23 18:44:49.000000'),(4,_binary '','2026-02-28 18:58:58.702824','Recuperare medicală bazată pe programe de exerciții fizice individualizate, menite să refacă funcțiile motrice, să crească mobilitatea articulară și să îmbunătățească forța musculară.','Kinetoterapie','2026-03-23 18:44:54.000000'),(5,_binary '','2026-03-23 18:45:12.000000','Tratamente ce utilizează agenți fizici (curenți electrici, ultrasunete, laser) pentru combaterea durerii, eliminarea contracturilor și stimularea circulației.','Fizioterapie','2026-03-23 18:45:12.000000'),(6,_binary '','2026-03-23 18:45:16.000000','Tehnologii avansate utilizate pentru accelerarea procesului de vindecare și reducerea inflamației.','Terapii Speciale','2026-03-23 18:45:16.000000'),(7,_binary '','2026-03-23 18:53:39.000000','Tehnici de manipulare a țesuturilor moi dedicate relaxării musculare, deblocării punctelor tensionate și îmbunătățirii stării generale de bine.','Masaj','2026-03-23 18:53:39.000000');
+INSERT INTO `tip_serviciu` VALUES (1,_binary '','2026-02-28 11:47:32.489525','Punctul de plecare pentru orice program de recuperare.','Evaluare','2026-03-23 18:44:49.000000','SYSTEM','SYSTEM'),(4,_binary '','2026-02-28 18:58:58.702824','Recuperare medicală bazată pe programe de exerciții fizice individualizate, menite să refacă funcțiile motrice, să crească mobilitatea articulară și să îmbunătățească forța musculară.','Kinetoterapie','2026-03-23 18:44:54.000000','SYSTEM','SYSTEM'),(5,_binary '','2026-03-23 18:45:12.000000','Tratamente ce utilizează agenți fizici (curenți electrici, ultrasunete, laser) pentru combaterea durerii, eliminarea contracturilor și stimularea circulației.','Fizioterapie','2026-03-23 18:45:12.000000','SYSTEM','SYSTEM'),(6,_binary '','2026-03-23 18:45:16.000000','Tehnologii avansate utilizate pentru accelerarea procesului de vindecare și reducerea inflamației.','Terapii Speciale','2026-03-23 18:45:16.000000','SYSTEM','SYSTEM'),(7,_binary '','2026-03-23 18:53:39.000000','Tehnici de manipulare a țesuturilor moi dedicate relaxării musculare, deblocării punctelor tensionate și îmbunătățirii stării generale de bine.','Masaj','2026-03-23 18:53:39.000000','SYSTEM','SYSTEM');
 /*!40000 ALTER TABLE `tip_serviciu` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -99,4 +99,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-05-13 13:39:54
+-- Dump completed on 2026-06-29 16:28:21
